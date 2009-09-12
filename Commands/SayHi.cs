aabe64b034dd024d99244ad69124bc57a802c50e
Simon Walker <github@helpmebot.org.uk>
2009-09-12 16:34:58 +0000

    add command folder, and sayhi command

---
+++ Commands/SayHi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using helpmebot6;
+
+namespace helpmebot6.Commands
+{
+    class SayHi
+    {
+        static string execute()
+        {
+            return Configuration.singleton.GetMessage( "cmdSayHi1" ,
+                    toUser
+                    );
+
+        }
+    }
+}
