7c7af3b60fc85bd6eb3312b5a44b4bb2f0e2140a
Simon Walker <github@helpmebot.org.uk>
2009-09-19 01:21:55 +0000

    update control file

--- helpmebot.sh
+++ helpmebot.sh
@@ -8,26 +8,18 @@ cd /home/stwalkerster/$COPY
 
 startbot() 
 {
-	rm bin/Helpmebot.$COPY.exe
-	cp bin/Helpmebot.exe bin/Helpmebot.$COPY.exe
-	mono bin/Helpmebot.$COPY.exe &> bin/log/`date +%Y-%m-%d--%H-%M` &
+	mono bin/Debug/helpmebot6.exe &> bin/log/`date +%Y-%m-%d--%H-%M` &
 }
 
 stopbot()
 {
-	PID=`ps -N -C "grep" -o pid,command | grep "bin/Helpmebot.$COPY.exe"  | awk -F" " '{ print $1 }' | tail -n 1`
+	PID=`ps -N -C "grep" -o pid,command | grep "bin/Debug/helpmebot6.exe"  | awk -F" " '{ print $1 }' | tail -n 1`
 	kill $PID 2> /dev/null
 }
 
 rebuildbot() 
 {
-	rm bin/Helpmebot.old.exe
-	mv bin/Helpmebot.exe bin/Helpmebot.old.exe
-	rm bin/MySql.Data.dll
-	cp MySql.Data.dll bin/MySql.Data.dll
-	rm bin/DotNetWikiBot.dll
-	cp DotNetWikiBot.dll bin/DotNetWikiBot.dll
-	gmcs -out:bin/Helpmebot.exe -reference:MySql.Data.dll -reference:DotNetWikiBot.dll -reference:/usr/lib/mono/2.0/System.Data.dll -reference:/usr/lib/mono/2.0/System.Web.dll -main:helpmebot6.Helpmebot6 Monitoring/WatcherController.cs Monitoring/CategoryWatcher.cs CommandParser.cs Configuration.cs ConfigurationSetting.cs DAL.cs GlobalFunctions.cs Helpmebot.cs IAL.cs NubioApi.cs User.cs WordLearner.cs
+	xbuild
 }
 
 updatebot() 
@@ -47,7 +39,7 @@ case $1 in
 		startbot
 	;;
 	restart)
-		PID=`ps -N -C "grep" -o pid,command | grep "bin/Helpmebot.$COPY.exe"  | awk -F" " '{ print $1 }' | tail -n 1`
+		PID=`ps -N -C "grep" -o pid,command | grep "bin/Debug/helpmebot6.exe"  | awk -F" " '{ print $1 }' | tail -n 1`
 		if [ "$PID" = "" ]; then
 			startbot	
         fi
