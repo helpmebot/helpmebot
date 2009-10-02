fe1b431b2387376d100176d423b5dc01fffc7d27
Simon Walker <github@helpmebot.org.uk>
2009-10-02 02:17:50 +0000

    implement cmdAge

--- Commands/Age.cs
+++ Commands/Age.cs
@@ -13,7 +13,20 @@ namespace helpmebot6.Commands
 
         protected override void execute( User source , string destination , string[ ] args )
         {
-            throw new NotImplementedException( );
+            if( args.Length > 0 )
+            {
+                string username = string.Join( " " , args );
+                TimeSpan time = getWikipedianAge( username );
+                string[ ] messageParameters = { username , time.Days , time.Hours , time.Minutes , time.Seconds };
+                string message = Configuration.Singleton( ).GetMessage( "cmdAge" , messageParameters );
+                IAL.singleton.IrcPrivmsg( destination , message );
+            }
+            else
+            {
+                string[ ] messageParameters = { "age" , "1" , args.Length.ToString( ) };
+                IAL.singleton.IrcNotice( source.Nickname , Configuration.Singleton( ).GetMessage( "notEnoughParameters" , messageParameters ) );
+
+            }
         }
 
         public TimeSpan getWikipedianAge( string userName )
