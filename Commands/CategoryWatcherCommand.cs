eaa55e3fa473c4fa3270fe9cfa20f1775794b6dc
Simon Walker <github@helpmebot.org.uk>
2009-11-05 00:50:46 +0000

    rename file (makes it easier for development, class name unchanged)

---
+++ Commands/CategoryWatcherCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class CategoryWatcher : GenericCommand
+    {
+        public CategoryWatcher( )
+        {
+            accessLevel = GlobalFunctions.commandAccessLevel( "categorywatcher" );
+        }
+
+        protected override CommandResponseHandler execute( User source , string channel , string[ ] args )
+        {
+            CommandResponseHandler crh = new CommandResponseHandler( );
+
+            if( args.Length == 1 )
+            { // just do category check
+                crh.respond( Monitoring.WatcherController.Instance( ).forceUpdate( args[ 0 ] ) );
+            }
+            else
+            { // do something else too.
+
+            }
+            return crh;
+        }
+        
+    }
+}
