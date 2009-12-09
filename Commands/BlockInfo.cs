a7e32ff179b02afc950546ae37f4f5db48bbcb3d
Simon Walker <github@helpmebot.org.uk>
2009-12-09 22:28:47 +0000

    implement block info command

--- Commands/BlockInfo.cs
+++ Commands/BlockInfo.cs
@@ -7,51 +7,87 @@ namespace helpmebot6.Commands
     /// <summary>
     /// Returns the block information of a wikipedian
     /// </summary>
-    class BlockInfo : GenericCommand
+    class Blockinfo : GenericCommand
     {
-        public BlockInfo( )
+        public Blockinfo( )
         {
             accessLevel = GlobalFunctions.commandAccessLevel( );
         }
 
         protected override CommandResponseHandler execute( User source , string channel , string[ ] args )
         {
-            throw new NotImplementedException( );
+            return new CommandResponseHandler( getBlockInformation( string.Join(" ", args)  ).ToString( ) );
         }
 
-        //bool isBlocked( string username )
-        //{
-        //    try
-        //    {
-        //        System.Xml.XmlTextReader creader = new System.Xml.XmlTextReader(
-        //                "http://en.wikipedia.org/w/api.php?format=xml&action=query&list=users&usprop=blockinfo&ususers=" + username );
-        //        do
-        //        {
-        //            creader.Read( );
-        //        } while( creader.Name != "user" );
-        //        string missing = creader.GetAttribute( "missing" );
-        //        if( missing == null )
-        //        {
-        //            string blockedby = creader.GetAttribute( "blockedby" );
-        //            if( blockedby == null )
-        //            {
-        //                return false;
-        //            }
-        //            else
-        //            {
-        //                return true;
-        //            }
-        //        }
-        //        else
-        //        {
-        //            return false;
-        //        }
-        //    }
-        //    catch( Exception ex )
-        //    {
-        //        sendMessage( debugIrcChannel , "***ERROR***: " + ex.ToString( ) + ": " + ex.Message , true , false );
-        //    }
-        //    return false;
-        //}
+        public BlockInformation getBlockInformation( string userName )
+        {
+            string baseWiki = Configuration.Singleton( ).retrieveGlobalStringOption( "baseWiki" );
+            string api = DAL.Singleton( ).ExecuteScalarQuery( "SELECT `site_api` FROM `site` WHERE `site_id` = " + baseWiki + ";" );
+            string apiParams = "?action=query&list=blocks&bkusers="+userName+"&format=xml";
+            System.Xml.XmlTextReader creader = new System.Xml.XmlTextReader( api + apiParams );
+
+            while( creader.Name != "blocks" )
+            {
+                creader.Read( );
+            }
+            creader.Read( );
+
+            if( creader.Name == "block" )
+            {
+                BlockInformation bi = new BlockInformation( );
+                bi.id = creader.GetAttribute( "id" );
+                bi.target = creader.GetAttribute( "user" );
+                bi.blockedBy = creader.GetAttribute( "by" );
+                bi.start = creader.GetAttribute( "timestamp" );
+                bi.expiry = creader.GetAttribute( "expiry" );
+                bi.blockReason = creader.GetAttribute( "reason" );
+
+                bi.autoblock = creader.GetAttribute( "autoblock" ) == "" ? true : false;
+                bi.nocreate = creader.GetAttribute( "nocreate" ) == "" ? true : false;
+                bi.noemail = creader.GetAttribute( "noemail" ) == "" ? true : false;
+
+                return bi;
+            }
+            else
+            {
+                return new BlockInformation( );
+            }
+        }
+
+        public struct BlockInformation
+        {
+            public string id;
+            public string target;
+            public string blockedBy;
+            public string blockReason;
+            public string expiry;
+            public string start;
+            public bool nocreate;
+            public bool autoblock;
+            public bool noemail;
+
+            public override string ToString( )
+            {
+                string[ ] emptyMessageParams = { "", "", "", "", "", "", "" };
+                string emptyMessage = Configuration.Singleton( ).GetMessage( "blockInfoShort", emptyMessageParams );
+                
+                string info = "";
+                if( nocreate )
+                    info += "NOCREATE ";
+                if( autoblock )
+                    info += "AUTOBLOCK ";
+                if( noemail )
+                    info += "NOEMAIL ";
+                string[ ] messageParams = { id, target, blockedBy, expiry, start, blockReason, info };
+                string message = Configuration.Singleton( ).GetMessage( "blockInfoShort", messageParams );
+
+                if( message == emptyMessage )
+                {
+                    message = Configuration.Singleton( ).GetMessage( "noBlocks" );
+                }
+
+                return message;
+            }
+        }
     }
 }
