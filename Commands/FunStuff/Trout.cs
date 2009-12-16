1980f8b8bb22fbcb095972cbe8b40fa71f263dc8
Simon Walker <github@helpmebot.org.uk>
2009-12-16 03:17:53 +0000

    add trout command

---
+++ Commands/FunStuff/Trout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot6.Commands.FunStuff
+{
+    class Trout  :GenericCommand
+    {
+        public Trout( )
+        {
+            accessLevel = GlobalFunctions.commandAccessLevel( );
+        }
+
+        protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
+        {
+            string name = string.Join( " ", args );
+
+            if( name.ToLower() == helpmebot6.Helpmebot6.irc.IrcNickname.ToLower() || name.ToLower() == "stwalkerster" )
+            {
+                name = source.Nickname;
+            }
+
+            string[ ] messageparams = { name };
+            string message = IAL.wrapCTCP( "ACTION", Configuration.Singleton( ).GetMessage( "cmdTrout", messageparams ) );
+
+            return new CommandResponseHandler( message );
+
+        }
+    }
+}
