4fe5791b77d6c3537c1a6a8070006f8eff778f0d
Simon Walker <github@helpmebot.org.uk>
2009-12-16 17:46:08 +0000

    start doing new year stuff

---
+++ NewYear/TimeMonitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot.NewYear
+{
+    class TimeMonitor
+    {
+        public static TimeMonitor instance( )
+        {
+            if( _instance == null )
+                _instance = new TimeMonitor( );
+
+            return _instance;
+        }
+        private static TimeMonitor _instance;
+
+        protected TimeMonitor( )
+        {
+
+        }
+    }
+}
