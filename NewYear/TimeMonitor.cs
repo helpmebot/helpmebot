bcbd5e673c77f7689c6b10cc91878f77de6e096a
Simon Walker <github@helpmebot.org.uk>
2009-12-23 02:49:16 +0000

    more work on time monitor

--- NewYear/TimeMonitor.cs
+++ NewYear/TimeMonitor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace helpmebot.NewYear
 {
@@ -13,10 +14,20 @@ namespace helpmebot.NewYear
 
             return _instance;
         }
+        
         private static TimeMonitor _instance;
+        private Thread monitorThread;
 
         protected TimeMonitor( )
         {
+            monitorThread = new Thread( new ThreadStart( monitorThreadMethod ) );
+            monitorThread.Start( );
+        }
+
+        private void monitorThreadMethod( )
+        {
+            // SELECT tz_places FROM timezones t WHERE ADDDATE("2010-01-01 00:00:00", INTERVAL -tz_offset_hours HOUR) = NOW();
+
 
         }
     }
