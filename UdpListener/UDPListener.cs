0d5bea1fdbf4e777c037a271b214b5a96f9fad24
Simon Walker <github@helpmebot.org.uk>
2010-01-08 04:43:56 +0000

    add udp listening subsystem

---
+++ UdpListener/UDPListener.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Security.Cryptography;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+namespace helpmebot6.UdpListener
+{
+    public class UDPListener
+    {
+        public UDPListener( int port )
+        {
+            key = Configuration.Singleton( ).retrieveGlobalStringOption( "udpKey" );
+            udpClient = new UdpClient( port );
+            listenerThread = new Thread( new ThreadStart( threadMethod ) );
+            listenerThread.Start( );
+        }
+
+        
+        Thread listenerThread;
+        UdpClient udpClient;
+        string key;
+
+        private void threadMethod( )
+        {
+            try
+            {
+                while( true )
+                {
+                    IPEndPoint ipep = new IPEndPoint( IPAddress.Any, 0 );
+                     byte[ ] datagram = udpClient.Receive( ref ipep );
+
+                    BinaryFormatter bf = new BinaryFormatter( );
+                    UdpMessage recievedMessage = (UdpMessage)bf.Deserialize( new MemoryStream( datagram ) );
+
+                    byte[ ] bm = ASCIIEncoding.ASCII.GetBytes( recievedMessage.Message + key );
+                    byte[ ] bh = MD5.Create( ).ComputeHash( bm );
+                    string hash = ASCIIEncoding.ASCII.GetString( bh );
+                    if( hash == recievedMessage.Hash )
+                    {
+                        Helpmebot6.irc.SendRawLine( "PRIVMSG " + recievedMessage.Message );
+                    }
+                }
+            }
+            catch( ThreadAbortException ex )
+            {
+                GlobalFunctions.ErrorLog( ex );
+            }
+        }
+    }
+}
---
+++ UdpListener/UdpMessage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+namespace helpmebot6.UdpListener
+{
+    public class  UdpMessage : ISerializable
+    {
+        public string Hash
+        {
+            get
+            {
+                return _hash;
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+        }
+
+        string _hash;
+        string _message;
+
+        public UdpMessage(string message, string key )
+        {
+            _message = message;
+            byte[ ] bm = ASCIIEncoding.ASCII.GetBytes( message + key );
+            byte[ ] bh = MD5.Create( ).ComputeHash( bm );
+            _hash = ASCIIEncoding.ASCII.GetString( bh );
+        }
+
+        #region ISerializable Members
+
+        public UdpMessage( SerializationInfo info, StreamingContext ctxt )
+        {
+            _hash = info.GetString( "hash" );
+            _message = info.GetString( "message" );
+        }
+
+        public void GetObjectData( SerializationInfo info, StreamingContext context )
+        {
+            info.AddValue( "hash", _hash );
+            info.AddValue( "message", _message );
+        }
+
+        #endregion
+    }
+}
