04f877e5dced23f6eec921b20317fdff6a399ca0
Simon Walker <github@helpmebot.org.uk>
2010-01-26 10:14:54 +0000

    Add new way of stopping all threaded operations. All threaded operations need to be adapted to use this new method.
    This method will make it each class utilising threads responsible for shutting itself down. Importantly, it will allow threaded classes to alert the rest of the system that they use a thread, which needs to be stopped before the bot can cleanly shut down.

---
+++ Threading/IThreadedSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot6.Threading
+{
+    public interface IThreadedSystem
+    {
+        /// <summary>
+        /// Stop all threads in this instance to allow for a clean shutdown.
+        /// </summary>
+        void Stop( );
+
+        /// <summary>
+        /// Register this instance of the threaded class with the global list
+        /// </summary>
+        void RegisterInstance( );
+    }
+}
---
+++ Threading/ThreadList.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace helpmebot6.Threading
+{
+    class ThreadList
+    {
+        private static ThreadList _instance;
+        public static ThreadList instance( )
+        {
+            if( _instance == null )
+                _instance = new ThreadList( );
+
+            return _instance;
+        }
+        protected ThreadList( )
+        {
+            threadedObjects = new ArrayList( );
+        }
+
+        ArrayList threadedObjects;
+
+        public void register( IThreadedSystem sender )
+        {
+            threadedObjects.Add( sender );
+        }
+
+        public void stop( )
+        {
+            foreach( object obj in threadedObjects )
+            {
+                ( (IThreadedSystem)obj ).Stop( );
+            }
+        }
+
+    }
+}
