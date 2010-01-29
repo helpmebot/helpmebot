a8dc42741836386fb9dfbedca46c10cb3aa7e258
Simon Walker <github@helpmebot.org.uk>
2010-01-29 00:17:26 +0000

    add threadstatus command

---
+++ Commands/ThreadStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using helpmebot6.Threading;
+
+namespace helpmebot6.Commands
+{
+    class ThreadStatus : GenericCommand
+    {
+        protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
+        {
+            string[ ] statuses = ThreadList.instance( ).getAllThreadStatus( );
+            CommandResponseHandler crh = new CommandResponseHandler( );
+            foreach( string item in statuses )
+            {
+                crh.respond( item );
+            }
+            return crh;
+        }
+    }
+}
