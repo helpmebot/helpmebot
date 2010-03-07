694a5ad402ebb267b69081a1a05fc35882615238
Simon Walker <github@helpmebot.org.uk>
2010-03-07 21:23:58 +0000

    fix

--- Commands/Decode.cs
+++ Commands/Decode.cs
@@ -9,20 +9,20 @@ namespace helpmebot6.Commands
     {
         protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
         {
-            if( args[ 1 ].Length != 8 )
+            if( args[ 0 ].Length != 8 )
                 return null;
 
             byte[ ] ip = new byte[ 4 ];
-            ip[ 0 ] = Convert.ToByte( args[ 1 ].Substring( 0, 2 ), 16 );
-            ip[ 1 ] = Convert.ToByte( args[ 1 ].Substring( 2, 2 ), 16 );
-            ip[ 2 ] = Convert.ToByte( args[ 1 ].Substring( 4, 2 ), 16 );
-            ip[ 3 ] = Convert.ToByte( args[ 1 ].Substring( 6, 2 ), 16 );
+            ip[ 0 ] = Convert.ToByte( args[ 0 ].Substring( 0, 2 ), 16 );
+            ip[ 1 ] = Convert.ToByte( args[ 0 ].Substring( 2, 2 ), 16 );
+            ip[ 2 ] = Convert.ToByte( args[ 0 ].Substring( 4, 2 ), 16 );
+            ip[ 3 ] = Convert.ToByte( args[ 0 ].Substring( 6, 2 ), 16 );
 
             IPAddress ipAddr = new IPAddress( ip );
 
             IPHostEntry iphe = Dns.GetHostEntry( ipAddr );
-            
-            string[] messageargs = { args[1], ipAddr.ToString(), iphe.HostName};
+
+            string[ ] messageargs = { args[ 0 ], ipAddr.ToString( ), iphe.HostName };
 
             return new CommandResponseHandler( Configuration.Singleton( ).GetMessage( "hexDecodeResult", messageargs ) );
         }
