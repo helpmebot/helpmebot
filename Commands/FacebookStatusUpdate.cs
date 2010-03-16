7b4f2c239d6527f234106d361fff8822bc73f327
Simon Walker <github@helpmebot.org.uk>
2010-03-16 02:52:56 +0000

    fb status update. bound to go wrong somewhere.

---
+++ Commands/FacebookStatusUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Facebookstatusupdate : GenericCommand
+    {
+        protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
+        {
+            string fbmail = Configuration.Singleton( ).retrieveGlobalStringOption( "fbEmail" );
+            System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage( "helpmebot@helpmebot.org.uk", fbmail );
+            message.Subject = string.Join( " ", args );
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient( "localhost" );
+            client.Send( message );
+            return;
+        }
+    }
+}
