14308abd2a9ddbd60cc12695de99306af56efd1e
Simon Walker <github@helpmebot.org.uk>
2010-03-16 04:40:40 +0000

    tweets!

---
+++ Commands/Tweet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace helpmebot6.Commands
+{
+    class Tweet : GenericCommand 
+    {
+
+        protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
+        {
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create( "http://api.twitter.com/1/statuses/update.xml" );
+            wr.Credentials = new NetworkCredential(
+                    Configuration.Singleton( ).retrieveGlobalStringOption( "twitterUsername" ),
+                    Configuration.Singleton( ).retrieveGlobalStringOption( "twitterPassword" )
+                );
+            wr.Method = "POST";
+            wr.UserAgent = Configuration.Singleton( ).retrieveGlobalStringOption( "useragent" );
+            string status = string.Join( " ", args );
+            status = "status=" + status.Substring( 0, 140 );
+            using( Stream writeStream = wr.GetRequestStream( ) )
+            {
+                UTF8Encoding encoding = new UTF8Encoding( );
+                byte[ ] bytes = encoding.GetBytes( status );
+                writeStream.Write( bytes, 0, bytes.Length );
+            }
+
+            HttpWebResponse wrsp = (HttpWebResponse)wr.GetResponse( );
+            if( wrsp.StatusCode == HttpStatusCode.OK )
+                return new CommandResponseHandler( Configuration.Singleton( ).GetMessage( "done" ) );
+            else
+                throw new Exception( );
+        }
+    }
+}
