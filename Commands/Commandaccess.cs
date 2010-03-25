1e02ad5159df0db99ecb135ce757d04e040325f1
Simon Walker <github@helpmebot.org.uk>
2010-03-25 09:28:00 +0000

    fix

--- Commands/Commandaccess.cs
+++ Commands/Commandaccess.cs
@@ -8,11 +8,10 @@ namespace helpmebot6.Commands
     {
         protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
         {
-            Type cmd = Type.GetType( "helpmebot6.Commands." + args[ 0 ] );
-            if( cmd.IsSubclassOf( Type.GetType( "helpmebot6.Commands.GenericCommand" ) ) )
-            {
+
+            Type cmd = Type.GetType( "helpmebot6.Commands." + args[ 0 ].Substring(0,1).ToUpper() + args[0].Substring(1).ToLower() );
+            if( cmd != null )
                 return new CommandResponseHandler( ( (GenericCommand)Activator.CreateInstance( cmd ) ).accessLevel.ToString( ) );
-            }
             else
                 return null;
         }
