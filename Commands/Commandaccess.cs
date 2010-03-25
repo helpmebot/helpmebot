aca6c5608d21c42f7969ea7f132b59d75dd82da7
Simon Walker <github@helpmebot.org.uk>
2010-03-25 09:18:17 +0000

    add commandaccess command

---
+++ Commands/Commandaccess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Commandaccess : GenericCommand
+    {
+        protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
+        {
+            Type cmd = Type.GetType( "helpmebot6.Commands." + args[ 0 ] );
+            if( cmd.IsSubclassOf( Type.GetType( "helpmebot6.Commands.GenericCommand" ) ) )
+            {
+                return new CommandResponseHandler( ( (GenericCommand)Activator.CreateInstance( cmd ) ).accessLevel.ToString( ) );
+            }
+        }
+    }
+}
