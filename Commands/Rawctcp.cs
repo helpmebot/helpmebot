7900ea197d0261de4dc2950f82b0eb0308c3a361
Simon Walker <github@helpmebot.org.uk>
2010-03-26 01:42:35 +0000

    add rawctcp command

---
+++ Commands/Rawctcp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Rawctcp : GenericCommand
+    {
+        protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
+        {
+            string cmd = GlobalFunctions.popFromFront( ref args );
+
+            Helpmebot6.irc.SendRawLine( IAL.wrapCTCP( cmd, string.Join( " ", args ) ) );
+
+            return null;
+        }
+    }
+}
