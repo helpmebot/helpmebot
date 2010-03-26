88f7d3fc835f06d01f853c3cc7bf3357d47f05f8
Simon Walker <github@helpmebot.org.uk>
2010-03-26 01:43:51 +0000

    fix

--- Commands/Rawctcp.cs
+++ Commands/Rawctcp.cs
@@ -9,8 +9,9 @@ namespace helpmebot6.Commands
         protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
         {
             string cmd = GlobalFunctions.popFromFront( ref args );
+            string dst = GlobalFunctions.popFromFront( ref args );
 
-            Helpmebot6.irc.SendRawLine( IAL.wrapCTCP( cmd, string.Join( " ", args ) ) );
+            Helpmebot6.irc.IrcPrivmsg( dst, IAL.wrapCTCP( cmd, string.Join( " ", args ) ) );
 
             return null;
         }
