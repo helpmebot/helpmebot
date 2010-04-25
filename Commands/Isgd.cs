1db847bb1f29ec139f58c6f27fbae59eed4ebc0e
Simon Walker <github@helpmebot.org.uk>
2010-04-25 01:26:28 +0000

    Add url shortening to bot

---
+++ Commands/Isgd.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Isgd:GenericCommand
+    {
+        protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
+        {
+            return new CommandResponseHandler( IsGd.shorten( new Uri( args[ 0 ] ) ).ToString( ) );
+        }
+    }
+}
---
+++ IsGd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace helpmebot6
+{
+    class IsGd
+    {
+        public static Uri shorten( Uri longUrl )
+        {
+            HttpWebRequest wrq = (HttpWebRequest)WebRequest.Create( "http://is.gd/api.php?longurl=" + longUrl.ToString( ) );
+            wrq.UserAgent = Configuration.Singleton( ).retrieveGlobalStringOption( "useragent" );
+            HttpWebResponse wrs = (HttpWebResponse)wrq.GetResponse( );
+            if( wrs.StatusCode == HttpStatusCode.OK )
+            {
+                StreamReader sr = new StreamReader( wrs.GetResponseStream( ) );
+                return new Uri( sr.ReadLine( ) );
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
