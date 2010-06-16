27d033140bf25b35017a14c42b6e79d34883aa0a
Simon Walker <github@helpmebot.org.uk>
2010-06-16 20:44:52 +0000

    helper command, temp fix for bug 60

---
+++ Commands/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Helper : GenericCommand
+    {
+        protected override CommandResponseHandler execute(User source, string channel, string[] args)
+        {
+            // FIXME: this needs putting into its own subsystem, messageifying, configifying, etc.
+
+            string message = "[HELP]: " + source.ToString() + " needs help in #wikipedia-en-help !";
+            if (args.Length > 0)
+                message += "(\"" + string.Join(" ", args) + "\")";
+
+            Helpmebot6.irc.IrcNotice("#wikipedia-en-helpers", message);
+
+            return null; 
+        }
+    }
+}
