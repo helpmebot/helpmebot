69a3488b1c302afb3f2c6ad271a87254a9369418
Simon Walker <github@helpmebot.org.uk>
2010-08-01 08:24:00 +0000

    comment out breakage

--- Commands/Afccount.cs
+++ Commands/Afccount.cs
@@ -14,19 +14,19 @@ namespace helpmebot6.Commands
 
             string baseWiki = Configuration.singleton().retrieveLocalStringOption("baseWiki", channel);
 
-            DAL.Select q = new DAL.Select("site_api");
-            q.setFrom("site");
-            q.addWhere(new DAL.WhereConds("site_id", baseWiki));
-            string api = DAL.singleton().executeScalarSelect(q);
+           // DAL.Select q = new DAL.Select("site_api");
+           // q.setFrom("site");
+           // q.addWhere(new DAL.WhereConds("site_id", baseWiki));
+           // string api = DAL.singleton().executeScalarSelect(q);
 
-            XPathDocument d =
-                new XPathDocument(
-                    HttpRequest.get( api +
-                                     "?format=xml&action=query&prop=categoryinfo&titles=Category:Pending%20AfC%20submissions" ) );
+           // XPathDocument d =
+           //     new XPathDocument(
+           //         HttpRequest.get( api +
+           //                          "?format=xml&action=query&prop=categoryinfo&titles=Category:Pending%20AfC%20submissions" ) );
             
 
-            XPathNavigator nav = d.CreateNavigator( );
-            XPathNodeIterator xpni = nav.Select( "//categoryinfo@pages" );
+           // XPathNavigator nav = d.CreateNavigator( );
+           // XPathNodeIterator xpni = nav.Select( "//categoryinfo@pages" );
 
             ;
 
