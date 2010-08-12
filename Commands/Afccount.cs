c902803b56e1d292055bfdd7a21a7b59ce83bbcd
Simon Walker <github@helpmebot.org.uk>
2010-08-12 21:41:57 +0000

    wtf conflicted file somehow got committed

--- Commands/Afccount.cs
+++ Commands/Afccount.cs
@@ -1,4 +1,3 @@
-<<<<<<< .working
 // /****************************************************************************
 //  *   This file is part of Helpmebot.                                        *
 //  *                                                                          *
@@ -61,42 +60,4 @@ namespace helpmebot6.Commands
 
         }
     }
-=======
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Xml;
-using System.Xml.XPath;
-
-namespace helpmebot6.Commands
-{
-    class Afccount : GenericCommand
-    {
-        protected override CommandResponseHandler execute( User source, string channel, string[ ] args )
-        {
-            //  api + ?format=xml&action=query&prop=categoryinfo&titles=Category:Pending%20AfC%20submissions
-
-            string baseWiki = Configuration.singleton().retrieveLocalStringOption("baseWiki", channel);
-
-           // DAL.Select q = new DAL.Select("site_api");
-           // q.setFrom("site");
-           // q.addWhere(new DAL.WhereConds("site_id", baseWiki));
-           // string api = DAL.singleton().executeScalarSelect(q);
-
-           // XPathDocument d =
-           //     new XPathDocument(
-           //         HttpRequest.get( api +
-           //                          "?format=xml&action=query&prop=categoryinfo&titles=Category:Pending%20AfC%20submissions" ) );
-            
-
-           // XPathNavigator nav = d.CreateNavigator( );
-           // XPathNodeIterator xpni = nav.Select( "//categoryinfo@pages" );
-
-            ;
-
-            return null;
-
-        }
-    }
->>>>>>> .merge-right.r448
 }
