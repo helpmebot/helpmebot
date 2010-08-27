20e4688c25ad87a6c6df3b6c424d4c7c9020f8f1
Simon Walker <github@helpmebot.org.uk>
2010-08-27 00:32:45 +0000

    Add nickserv login thing, rss monitor thing - both broken but seem to be OK Also added Vorticough easter egg :)

---
+++ Commands/FunStuff/Vorticough.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Vorticough : GenericCommand
+    {
+        protected override CommandResponseHandler execute(User source, string channel, string[] args)
+        {
+            return new CommandResponseHandler(new Message().get("Vortigaunt"));
+        }
+    }
+}
