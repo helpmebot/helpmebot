573de7ff11952428da6eeac21c38790acfa7963d
Simon Walker <github@helpmebot.org.uk>
2010-10-19 02:24:51 +0000

    Add a couple'la unit tests too

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -49,3 +49,5 @@ using System.Resources;
 [assembly: AssemblyVersion( "6.0.0.0" )]
 [assembly: AssemblyFileVersion( "6.0.0.0" )]
 [assembly: NeutralResourcesLanguageAttribute( "" )]
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("HelpmebotUnitTests")]
