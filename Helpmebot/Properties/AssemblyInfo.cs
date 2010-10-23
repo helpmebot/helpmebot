75232f9127784f8b75fb5e56a670adde7656a738
Simon Walker <github@helpmebot.org.uk>
2010-10-23 16:26:40 +0000

    add the unit tests back into subversion

--- Helpmebot/Properties/AssemblyInfo.cs
+++ Helpmebot/Properties/AssemblyInfo.cs
@@ -49,3 +49,4 @@ using System.Resources;
 [assembly: AssemblyVersion( "6.0.0.0" )]
 [assembly: AssemblyFileVersion( "6.0.0.0" )]
 [assembly: NeutralResourcesLanguageAttribute( "" )]
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("HelpmebotUnitTests")]
