ba30885de6fca29e52cc93c77d26c05978555c9e
Simon Walker <github@helpmebot.org.uk>
2010-10-31 04:39:14 +0000

    add ircproxy class

---
+++ Helpmebot/IrcProxy.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace helpmebot6
+{
+    class IrcProxy : Threading.IThreadedSystem
+    {
+        public IrcProxy(IAL baseIrcAccessLayer, uint port)
+        {
+            _base = baseIrcAccessLayer;
+            _port = port;
+        }
+
+        private IAL _base;
+        private uint _port;
+
+        #region IThreadedSystem members
+
+        public void stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void registerInstance()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] getThreadStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event EventHandler threadFatalError;
+        #endregion
+    }
+}
