547e3376a5ef58701b222dc94e7325cf85fdb4f4
JoeGazz <joegazz@example.com>
2010-11-14 22:29:56 +0000

    fixed bug 00074

--- Helpmebot/Commands/GenericCommand.cs
+++ Helpmebot/Commands/GenericCommand.cs
@@ -124,7 +124,14 @@ namespace helpmebot6.Commands
             response.respond(new Message().get("accessDenied", ""),
                              CommandResponseDestination.PrivateMessage);
             this.log("Access denied to command.");
-            AccessLog.instance().save(new AccessLog.AccessLogEntry(source, GetType(), false, channel, args));
+            if(!AccessLog.instance().save(new AccessLog.AccessLogEntry(source, GetType(), true,channel, args)))
+            {
+                CommandResponseHandler errorResponse = new CommandResponseHandler();
+                errorResponse.respond("Error adding to access log - command aborted.", CommandResponseDestination.ChannelDebug);
+                errorResponse.respond(new Message().get("AccessDeniedAccessListFailure"), CommandResponseDestination.Default);
+                return errorResponse;
+            }
+
             return response;
         }
 
