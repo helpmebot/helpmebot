f35869169654982b147bc392214f562aad18c0e0
Simon Walker <github@helpmebot.org.uk>
2010-11-20 03:11:22 +0000

    test for empty stringtoo

--- Helpmebot/Commands/FunStuff/Charge.cs
+++ Helpmebot/Commands/FunStuff/Charge.cs
@@ -9,7 +9,7 @@ namespace helpmebot6.Commands
     {
         protected override CommandResponseHandler execute(User source, string channel, string[] args)
         {
-            if (args.Length > 0)
+            if (args.Length > 0 && args[0] != string.Empty)
                 return new CommandResponseHandler(IAL.wrapCTCP("ACTION", new Message().get("cmdChargeParam", args[0])));
             return new CommandResponseHandler(IAL.wrapCTCP("ACTION", new Message().get("cmdCharge")));
         }
