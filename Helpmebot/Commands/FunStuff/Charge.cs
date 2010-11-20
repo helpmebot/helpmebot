f1b0e46702e5978b4985731419a7a18ab7ee68b5
Simon Walker <github@helpmebot.org.uk>
2010-11-20 03:09:45 +0000

    add charge command

---
+++ Helpmebot/Commands/FunStuff/Charge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Charge : FunStuff.FunCommand
+    {
+        protected override CommandResponseHandler execute(User source, string channel, string[] args)
+        {
+            if (args.Length > 0)
+                return new CommandResponseHandler(IAL.wrapCTCP("ACTION", new Message().get("cmdChargeParam", args[0])));
+            return new CommandResponseHandler(IAL.wrapCTCP("ACTION", new Message().get("cmdCharge")));
+        }
+    }
+}
