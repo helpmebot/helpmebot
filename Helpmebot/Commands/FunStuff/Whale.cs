2bd87b3d2ff872c9fb2acc2461c5ae876ff05ed7
Simon Walker <github@helpmebot.org.uk>
2010-11-24 03:00:13 +0000

    add whale command

---
+++ Helpmebot/Commands/FunStuff/Whale.cs
@@ -0,0 +1,35 @@
+﻿// /****************************************************************************
+//  *   This file is part of Helpmebot.                                        *
+//  *                                                                          *
+//  *   Helpmebot is free software: you can redistribute it and/or modify      *
+//  *   it under the terms of the GNU General Public License as published by   *
+//  *   the Free Software Foundation, either version 3 of the License, or      *
+//  *   (at your option) any later version.                                    *
+//  *                                                                          *
+//  *   Helpmebot is distributed in the hope that it will be useful,           *
+//  *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+//  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+//  *   GNU General Public License for more details.                           *
+//  *                                                                          *
+//  *   You should have received a copy of the GNU General Public License      *
+//  *   along with Helpmebot.  If not, see <http://www.gnu.org/licenses/>.     *
+//  ****************************************************************************/
+#region Usings
+
+using System.Reflection;
+
+#endregion
+
+namespace helpmebot6.Commands
+{
+    /// <summary>
+    /// Trouts a user
+    /// </summary>
+    internal class Whale : Trout
+    {
+        public Whale()
+        {
+            this.messageName = "CmdWhale";
+        }
+    }
+}
