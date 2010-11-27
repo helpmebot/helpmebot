f8f10e31114d91ce3d0800b46eabae4d9f97a793
Simon Walker <github@helpmebot.org.uk>
2010-11-27 19:00:15 +0000

    add lick command

---
+++ Helpmebot/Commands/FunStuff/Lick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Lick : FunStuff.FunCommand
+    {
+        protected override CommandResponseHandler execute(User source, string channel, string[] args)
+        {
+            string name = string.Join(" ", args);
+
+            string[] messageparams = { name };
+            string message = new Message().get("CmdLink", messageparams);
+
+            return new CommandResponseHandler(message);
+        }
+    }
+}
