e8eac7990e253e935944c3704343c1261d7773c6
Simon Walker <github@helpmebot.org.uk>
2011-02-02 23:37:58 +0000

    add log command for adminlog

---
+++ Helpmebot/Commands/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Log : GenericCommand
+    {
+        protected override CommandResponseHandler execute(User source, string channel, string[] args)
+        {
+            DAL.singleton().insert("adminlog", "", "", source.nickname, string.Join(" ", args));
+            return new CommandResponseHandler("Logged the message, Master");
+        }
+    }
+}
