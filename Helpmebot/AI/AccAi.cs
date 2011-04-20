7b8c883328e03403a067686044ce3ee2aebe8ce1
Simon Walker <github@helpmebot.org.uk>
2011-04-20 13:38:34 +0000

    change channel and add an extra check

--- Helpmebot/AI/AccAi.cs
+++ Helpmebot/AI/AccAi.cs
@@ -10,15 +10,16 @@ namespace helpmebot6.AI
         public static void checkCiaVcCommits(User source, string destination, string message)
         {
             if(source.nickname.Substring(0,4) == "CIA-")
-                if(destination=="#wikipedia-en-accounts")
+                if(destination=="#wikipedia-en-accounts-devs")
                 {
                     if (message.Contains("wars:"))
-                    {
-                        Helpmebot6.irc.ircPrivmsg("#wikipedia-en-accounts", "!rewrite-pull");
-                    }
+                        if (message.Contains("master"))
+                        {
+                            Helpmebot6.irc.ircPrivmsg("#wikipedia-en-accounts-devs", "!rewrite-pull");
+                        }
                     if (message.Contains("wp-en-acc:"))
                     {
-                        Helpmebot6.irc.ircPrivmsg("#wikipedia-en-accounts", "!sand-svnup");
+                        Helpmebot6.irc.ircPrivmsg("#wikipedia-en-accounts-devs", "!sand-svnup");
                     }
                 }
         }
