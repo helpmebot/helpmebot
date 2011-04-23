2b97451124c18b211ed0005764d5b5ef3b630a6b
Simon Walker <github@helpmebot.org.uk#>
2011-04-23 01:23:18 +0000

    more stable

--- Helpmebot/AI/AccAi.cs
+++ Helpmebot/AI/AccAi.cs
@@ -1,12 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace helpmebot6.AI
 {
-    class AccAi
+    public class AccAi
     {
+
+        public static bool activepull = false;
+
         public static void checkCiaVcCommits(User source, string destination, string message)
         {
             if(source.nickname.Substring(0,4) == "CIA-")
@@ -15,14 +18,26 @@ namespace helpmebot6.AI
                     if (message.Contains("wars:"))
                         if (message.Contains("master"))
                         {
-                            Helpmebot6.irc.ircPrivmsg("#wikipedia-en-accounts-devs", "!rewrite-pull");
+                            if (!activepull)
+                            {
+                                Helpmebot6.irc.ircPrivmsg("#wikipedia-en-accounts-devs", "!rewrite-pull");
+                                activepull = true;
+                            }
                         }
                     if (message.Contains("wp-en-acc:"))
                     {
                         Helpmebot6.irc.ircPrivmsg("#wikipedia-en-accounts-devs", "!sand-svnup");
                     }
                 }
+            if (source.nickname.Substring(0, 4) == "ACCBot")
+                if (destination == "#wikipedia-en-accounts-devs")
+                {
+                    if (message.Contains("http://toolserver.org/~acc/rewrite/acc.php"))
+                        activepull = false;
+                }
+
         }
 
     }
 }
+
