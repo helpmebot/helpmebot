620de2e2531187f4d9a96316b10fa1a4a0b3b7ae
Simon Walker <github@stwalkerster.co.uk>
2013-05-24 01:13:22 +0200

    nuke *is* a subclass of whale, right Deskana? ;)

---
+++ Helpmebot/Commands/FunStuff/Nuke.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace helpmebot6.Commands
+{
+    class Nuke : Whale // nuke *is* a subclass of whale, right Deskana? ;)
+    {
+        public Nuke()
+        {
+            this.messageName = "CmdNuke"; 
+        }
+    }
+}
