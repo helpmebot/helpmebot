a2af3ab059202459fed698d6b4078e36fddac464
Simon Walker <github@stwalkerster.co.uk>
2013-06-23 23:47:03 +0100

    add {command} and {n*} parameters

--- Helpmebot/ExtensionMethods/FormatWithExtension.cs
+++ Helpmebot/ExtensionMethods/FormatWithExtension.cs
@@ -6,24 +6,6 @@
 
     public static class FormatWithExtension
     {
-
-        public static string FormatWith(this string format, object source, string[] data)
-        {
-            IDictionary<string, object> dict = new Dictionary<string, object>();
-
-            foreach (var prop in source.GetType().GetProperties())
-            {
-                dict.Add(prop.Name, prop.GetValue(source, null));
-            }
-
-            for (int i = 0; i < data.Length; i++)
-            {
-                dict.Add(i.ToString(), data[i]);
-            }
-
-            return FormatWith(format, dict);
-        }
-
         public static string FormatWith(this string format, IDictionary<string,object> source)
         {
             if (format == null)
