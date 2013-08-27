391ba2bd251b23ab0bbfe15b00a5ab0e560b48d4
Donald J. Fortier II <Technical_13@yahoo.com>
2013-08-27 15:45:28 -0400

    Cleaned up.

--- Helpmebot/Commands/FunStuff/Coffee.cs
+++ Helpmebot/Commands/FunStuff/Coffee.cs
@@ -9,18 +9,16 @@ namespace helpmebot6.Commands
     {
         protected override CommandResponseHandler execute(User source, string channel, string[] args)
         {
-			string name = string.Join(" ", args);
-			
-			if( args.Length == 0 ) {
-				name = source.nickname;
-			} else {
-				name = string.Join(" ", args);
-			}
-			
-			string[] messageparams = { name };
-			string message = new Message().get("cmdCoffee", messageparams);
-			
-			return new CommandResponseHandler(message);
+            if( args.Length == 0 ) {
+                name = source.nickname;
+            } else {
+                name = string.Join(" ", args);
+            }
+            
+            string[] messageparams = { name };
+            string message = new Message().get("cmdCoffee", messageparams);
+            
+            return new CommandResponseHandler(message);
         }
     }
 }
