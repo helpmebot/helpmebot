3b2c2e01f8f78c71aab6a69e63a77dd5d2e4184f
Simon Walker <github@stwalkerster.co.uk>
2013-10-10 23:46:03 +0100

    start of new irc layer

---
+++ src/Helpmebot/IRC/IrcClient.cs
@@ -0,0 +1,88 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IrcClient.cs" company="Helpmebot Development Team">
+//   Helpmebot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   Helpmebot is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
+// </copyright>
+// <summary>
+//   Defines the IrcClient type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Helpmebot.IRC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Web.UI;
+
+    using Helpmebot.IRC.Events;
+
+    /// <summary>
+    /// The IRC client.
+    /// </summary>
+    public class IrcClient
+    {
+
+        /// <summary>
+        /// The network client.
+        /// </summary>
+        private readonly NetworkClient networkClient;
+
+        /// <summary>
+        /// The data interception function.
+        /// </summary>
+        private bool connectionRegistered = false;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="IrcClient"/> class.
+        /// </summary>
+        /// <param name="hostname">
+        /// The hostname.
+        /// </param>
+        /// <param name="port">
+        /// The port.
+        /// </param>
+        public IrcClient(string hostname, int port, string nickname, string username, string realName, string password)
+        {
+            this.Nickname = nickname;
+            this.networkClient = new NetworkClient(hostname, port);
+            this.networkClient.DataReceived += this.NetworkClientOnDataReceived;
+
+            this.RegisterConnection(username, realName, password);
+        }
+
+
+        /// <summary>
+        /// Gets or sets the nickname.
+        /// </summary>
+        public string Nickname { get; set; }
+
+        private void NetworkClientOnDataReceived(object sender, DataReceivedEventArgs dataReceivedEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RegisterConnection(string username, string realName, string password)
+        {
+            var registrationMessages = new List<string>
+                                           {
+                                               string.Format("CAP LS"),
+                                               string.Format("USER {0} * * :{1}", username, realName),
+                                               string.Format("PASS {0}", password),
+                                               string.Format("NICK {0}", this.Nickname)
+                                           };
+
+            this.networkClient.Send(registrationMessages);
+        }
+
+    }
+}
---
+++ src/Helpmebot/IRC/NetworkClient.cs
@@ -0,0 +1,244 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NetworkClient.cs" company="Helpmebot Development Team">
+//   Helpmebot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   Helpmebot is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
+// </copyright>
+// <summary>
+//   Defines the NetworkClient type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Helpmebot.IRC
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net.Sockets;
+    using System.Threading;
+
+    using Helpmebot.IRC.Events;
+
+    /// <summary>
+    /// The TCP client.
+    /// </summary>
+    /// <para>
+    /// This is an event-based asynchronous TCP client
+    /// </para>
+    public class NetworkClient
+    {
+        /// <summary>
+        /// The client.
+        /// </summary>
+        private readonly TcpClient client;
+
+        /// <summary>
+        /// The reader.
+        /// </summary>
+        private readonly StreamReader reader;
+
+        /// <summary>
+        /// The writer.
+        /// </summary>
+        private readonly StreamWriter writer;
+
+        /// <summary>
+        /// The reset event.
+        /// </summary>
+        private readonly AutoResetEvent writerThreadResetEvent;
+
+        /// <summary>
+        /// The send queue.
+        /// </summary>
+        private readonly Queue<string> sendQueue;
+
+        /// <summary>
+        /// The send queue lock.
+        /// </summary>
+        private readonly object sendQueueLock = new object();
+
+        /// <summary>
+        /// The hostname.
+        /// </summary>
+        private readonly string hostname;
+
+        /// <summary>
+        /// The port.
+        /// </summary>
+        private readonly int port;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="NetworkClient"/> class.
+        /// </summary>
+        /// <param name="hostname">
+        /// The hostname.
+        /// </param>
+        /// <param name="port">
+        /// The port.
+        /// </param>
+        public NetworkClient(string hostname, int port)
+        {
+            this.hostname = hostname;
+            this.port = port;
+            this.client = new TcpClient(this.hostname, this.port);
+
+            this.reader = new StreamReader(this.client.GetStream());
+            this.writer = new StreamWriter(this.client.GetStream());
+            this.sendQueue = new Queue<string>();
+
+            this.writerThreadResetEvent = new AutoResetEvent(true);
+
+            Thread readerThread = new Thread(this.ReaderThreadTask);
+            Thread writerThread = new Thread(this.WriterThreadTask);
+
+            readerThread.Start();
+            writerThread.Start();
+        }
+
+        /// <summary>
+        /// The data received.
+        /// </summary>
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+        /// <summary>
+        /// Gets the hostname.
+        /// </summary>
+        public string Hostname
+        {
+            get
+            {
+                return this.hostname;
+            }
+        }
+
+        /// <summary>
+        /// Gets the port.
+        /// </summary>
+        public int Port
+        {
+            get
+            {
+                return this.port;
+            }
+        }
+
+        /// <summary>
+        /// The send.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        public void Send(string message)
+        {
+            lock (this.sendQueueLock)
+            {
+                this.sendQueue.Enqueue(message);
+            }
+
+            this.writerThreadResetEvent.Set();
+        }
+
+        /// <summary>
+        /// The send.
+        /// </summary>
+        /// <param name="messages">
+        /// The messages.
+        /// </param>
+        public void Send(IEnumerable<string> messages)
+        {
+            lock (this.sendQueueLock)
+            {
+                foreach (var message in messages)
+                {
+                    this.sendQueue.Enqueue(message);
+                }
+            }
+
+            this.writerThreadResetEvent.Set();
+        }
+
+        /// <summary>
+        /// The disconnect.
+        /// </summary>
+        public void Disconnect()
+        {
+            this.writer.Flush();
+            this.writer.Close();
+            this.client.Close();
+        }
+
+        /// <summary>
+        /// The on data received.
+        /// </summary>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        protected virtual void OnDataReceived(DataReceivedEventArgs e)
+        {
+            EventHandler<DataReceivedEventArgs> handler = this.DataReceived;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        /// <summary>
+        /// The reader thread task.
+        /// </summary>
+        private void ReaderThreadTask()
+        {
+            while (this.client.Connected)
+            {
+                string data = this.reader.ReadLine();
+
+                if (data != null)
+                {
+                    this.OnDataReceived(new DataReceivedEventArgs(data));
+                }
+            }
+        }
+
+        /// <summary>
+        /// The writer thread task.
+        /// </summary>
+        private void WriterThreadTask()
+        {
+            while (this.client.Connected)
+            {
+                string item = null;
+
+                // grab an item from the queue if we can
+                lock (this.sendQueueLock)
+                {
+                    if (this.sendQueue.Count > 0)
+                    {
+                        item = this.sendQueue.Dequeue();
+                    }
+                }
+
+                if (item == null)
+                {
+                    // Wait here for an item to be added to the queue
+                    this.writerThreadResetEvent.WaitOne();
+                }
+                else
+                {
+                    this.writer.WriteLine(item);
+                    this.writer.Flush();
+
+                    // Flood protection
+                    Thread.Sleep(100);
+                }
+            }
+        }
+    }
+}
