08c78aecd282392e542cfc260bb04bc4d01459d3
Simon Walker <github@stwalkerster.co.uk>
2013-12-15 04:28:13 +0000

    unit test project

---
+++ src/Helpmebot.Tests/Services/MessageService.cs
@@ -0,0 +1,20 @@
+﻿namespace Helpmebot.Tests.Services
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class MessageService
+    {
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.Pass("woo");
+        }
+
+        [Test]
+        public void ExampleFail()
+        {
+            Assert.Fail("boo");
+        }
+    }
+}
