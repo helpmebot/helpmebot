de45a3cba6caf8ade6320e63450a98005304eea2
Simon Walker <github@stwalkerster.co.uk>
2013-12-17 01:07:16 +0000

    Common assembly info, and bump version :)

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿// --------------------------------------------------------------------------------------------------------------------
-// <copyright file="AssemblyInfo.cs" company="Helpmebot Development Team">
+// <copyright file="CommonAssemblyInfo.cs" company="Helpmebot Development Team">
 //   Helpmebot is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation, either version 3 of the License, or
@@ -14,25 +14,16 @@
 //   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
 // </copyright>
 // <summary>
-//   AssemblyInfo.cs
+//   CommonAssemblyInfo.cs
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
 using System.Reflection;
-using System.Runtime.InteropServices;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Helpmebot.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("88b77be4-800e-43b9-b19e-277c87cebb70")]
+[assembly: AssemblyCompany("Simon Walker")]
+[assembly: AssemblyProduct("Helpmebot")]
+[assembly: AssemblyCopyright("Copyright Simon Walker (c) 2009-2013 Some Rights Reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("6.3.*")]
+[assembly: NeutralResourcesLanguage("en-GB")]
