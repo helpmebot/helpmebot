eb7d11eac4111b3d6bc7b668118ddda7e0de892b
Simon Walker <github@stwalkerster.co.uk>
2013-12-23 14:01:23 +0000

    new command classes

---
+++ src/Helpmebot/Services/AccessLogService.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AccessLogService.cs" company="Helpmebot Development Team">
+//   Helpmebot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   Helpmebot is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
+// </copyright>
+// <summary>
+//   Defines the AccessLogService type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Helpmebot.Services
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Helpmebot.Model.Interfaces;
+    using Helpmebot.Services.Interfaces;
+
+    /// <summary>
+    /// The access log service.
+    /// </summary>
+    public class AccessLogService : IAccessLogService
+    {
+        /// <summary>
+        /// The success.
+        /// </summary>
+        /// <param name="user">
+        /// The user.
+        /// </param>
+        /// <param name="command">
+        /// The command.
+        /// </param>
+        /// <param name="arguments">
+        /// The arguments.
+        /// </param>
+        public void Success(IUser user, Type command, IEnumerable<string> arguments)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The failure.
+        /// </summary>
+        /// <param name="user">
+        /// The user.
+        /// </param>
+        /// <param name="command">
+        /// The command.
+        /// </param>
+        /// <param name="arguments">
+        /// The arguments.
+        /// </param>
+        public void Failure(IUser user, Type command, IEnumerable<string> arguments)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Helpmebot/Services/UserFlagService.cs
@@ -0,0 +1,89 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UserFlagService.cs" company="Helpmebot Development Team">
+//   Helpmebot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   Helpmebot is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
+// </copyright>
+// <summary>
+//   Defines the UserFlagService type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Helpmebot.Services
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Helpmebot.Model;
+    using Helpmebot.Model.Interfaces;
+    using Helpmebot.Repositories.Interfaces;
+    using Helpmebot.Services.Interfaces;
+
+    /// <summary>
+    /// The user flag service.
+    /// </summary>
+    public class UserFlagService : IUserFlagService
+    {
+        /// <summary>
+        /// The user repository.
+        /// </summary>
+        private readonly IUserRepository userRepository;
+
+        /// <summary>
+        /// The flag group repository.
+        /// </summary>
+        private readonly IFlagGroupRepository flagGroupRepository;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="UserFlagService"/> class.
+        /// </summary>
+        /// <param name="userRepository">
+        /// The user repository.
+        /// </param>
+        /// <param name="flagGroupRepository">
+        /// The flag group repository.
+        /// </param>
+        public UserFlagService(IUserRepository userRepository, IFlagGroupRepository flagGroupRepository)
+        {
+            this.userRepository = userRepository;
+            this.flagGroupRepository = flagGroupRepository;
+        }
+
+        /// <summary>
+        /// The get flags for user.
+        /// </summary>
+        /// <param name="user">
+        /// The user.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IEnumerable{String}"/>.
+        /// </returns>
+        public IEnumerable<string> GetFlagsForUser(IUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// The get flag group.
+        /// </summary>
+        /// <param name="name">
+        /// The name.
+        /// </param>
+        /// <returns>
+        /// The <see cref="FlagGroup"/>.
+        /// </returns>
+        public FlagGroup GetFlagGroup(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
