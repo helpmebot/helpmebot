3c389f3df31d58683d490f4d0bee591da269cb81
Simon Walker <github@stwalkerster.co.uk>
2014-01-11 23:53:17 +0000

    Add more files

---
+++ src/Helpmebot/IRC/Events/MessageReceivedEventArgs.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MessageReceivedEventArgs.cs" company="Helpmebot Development Team">
+//   Helpmebot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   Helpmebot is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
+// </copyright>
+// <summary>
+//   Defines the MessageReceivedEventArgs type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Helpmebot.IRC.Events
+{
+    using System;
+
+    using Helpmebot.IRC.Messages;
+
+    /// <summary>
+    /// The message received event args.
+    /// </summary>
+    public class MessageReceivedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="MessageReceivedEventArgs"/> class.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        public MessageReceivedEventArgs(IMessage message)
+        {
+            this.Message = message;
+        }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        public IMessage Message { get; set; }
+    }
+}
