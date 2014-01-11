6a784e220543b59512fbaeb1283d10d3e956a776
Simon Walker <github@stwalkerster.co.uk>
2014-01-11 23:54:38 +0000

    More work on the IRC client

--- src/Helpmebot/IRC/IrcClient.cs
+++ src/Helpmebot/IRC/IrcClient.cs
@@ -20,8 +20,11 @@
 
 namespace Helpmebot.IRC
 {
+    using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Threading;
 
     using Castle.Core.Logging;
 
@@ -29,6 +32,8 @@ namespace Helpmebot.IRC
     using Helpmebot.IRC.Events;
     using Helpmebot.IRC.Interfaces;
     using Helpmebot.IRC.Messages;
+    using Helpmebot.IRC.Model;
+    using Helpmebot.Model.Interfaces;
 
     /// <summary>
     /// The IRC client.
@@ -68,6 +73,21 @@ namespace Helpmebot.IRC
         private readonly List<string> clientCapabilities;
 
         /// <summary>
+        /// The user cache.
+        /// </summary>
+        private readonly Dictionary<string, IrcUser> userCache;
+
+        /// <summary>
+        /// The channels.
+        /// </summary>
+        private readonly Dictionary<string, Channel> channels;
+
+        /// <summary>
+        /// The connection registration semaphore.
+        /// </summary>
+        private readonly Semaphore connectionRegistrationSemaphore;
+
+        /// <summary>
         /// The cap multi prefix.
         /// </summary>
         private bool capMultiPrefix;
@@ -96,6 +116,16 @@ namespace Helpmebot.IRC
         /// The nickname.
         /// </summary>
         private string nickname;
+
+        /// <summary>
+        /// Is the client logged in to a nickserv account?
+        /// </summary>
+        private bool loggedIn;
+
+        /// <summary>
+        /// The server prefix.
+        /// </summary>
+        private string serverPrefix;
 
         #endregion
 
@@ -129,11 +159,24 @@ namespace Helpmebot.IRC
             this.realName = realName;
             this.password = password;
             this.networkClient.DataReceived += this.NetworkClientOnDataReceived;
+            this.ReceivedMessage += this.OnMessageReceivedEvent;
 
-            this.clientCapabilities = new List<string> { "sasl"/*, "account-notify", "extended-join", "multi-prefix"*/ };
+            this.clientCapabilities = new List<string> { "sasl", "account-notify", "extended-join", "multi-prefix" };
+
+            this.userCache = new Dictionary<string, IrcUser>();
+            this.channels = new Dictionary<string, Channel>();
+
+            this.connectionRegistrationSemaphore = new Semaphore(0, 1);
 
             this.RegisterConnection(null);
         }
+
+        /// <summary>
+        /// The received message.
+        /// </summary>
+        public event EventHandler<MessageReceivedEventArgs> ReceivedMessage;
+
+        #region Properties
 
         /// <summary>
         /// Gets or sets the nickname.
@@ -151,6 +194,30 @@ namespace Helpmebot.IRC
                 this.Send(new Message { Command = "NICK", Parameters = value.ToEnumerable() });
             }
         }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// The join.
+        /// </summary>
+        /// <param name="channel">
+        /// The channel.
+        /// </param>
+        public void Join(string channel)
+        {
+            if (channel == "0")
+            {
+                return;
+            }
+
+            this.connectionRegistrationSemaphore.WaitOne();
+
+            this.Send(new Message { Command = "JOIN", Parameters = channel.ToEnumerable() });
+        }
+
+        #endregion
 
         /// <summary>
         /// The send.
@@ -176,6 +243,19 @@ namespace Helpmebot.IRC
         {
             var message = Message.Parse(dataReceivedEventArgs.Data);
 
+            if (message.Command == "ERROR")
+            {
+                var errorMessage = message.Parameters.First();
+                this.logger.Fatal(errorMessage);
+                this.networkClient.Disconnect();
+                throw new IOException(errorMessage);
+            }
+
+            if (message.Command == "PING")
+            {
+                this.Send(new Message { Command = "PONG", Parameters = message.Parameters });
+            }
+
             if (this.connectionRegistered)
             {
                 this.RaiseDataEvent(message);
@@ -194,7 +274,133 @@ namespace Helpmebot.IRC
         /// </param>
         private void RaiseDataEvent(IMessage message)
         {
+            EventHandler<MessageReceivedEventArgs> temp = this.ReceivedMessage;
+            if (temp != null)
+            {
+                temp(this, new MessageReceivedEventArgs(message));
+            }
         }
+
+        /// <summary>
+        /// The on message received event.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void OnMessageReceivedEvent(object sender, MessageReceivedEventArgs e)
+        {
+            IUser user = null;
+            if (e.Message.Prefix != null)
+            {
+                if (e.Message.Prefix == this.serverPrefix)
+                {
+                    user = new ServerUser();
+                }
+                else
+                {
+                    user = IrcUser.FromPrefix(e.Message.Prefix);
+                }
+            }
+
+            if (e.Message.Command == "JOIN" && user != null)
+            {
+                // this is a client join to a channel.
+                // :stwalkerster!stwalkerst@wikimedia/stwalkerster JOIN ##stwalkerster
+                var parametersList = e.Message.Parameters.ToList();
+                
+                if (this.capExtendedJoin)
+                {
+                    // :stwalkerster!stwalkerst@wikimedia/stwalkerster JOIN ##stwalkerster accountname :realname
+                    user.Account = parametersList[1];
+                }
+
+                var channelName = parametersList[0];
+                if (user.Nickname == this.Nickname)
+                {
+                    this.logger.InfoFormat("Joining channel {0}", channelName);
+                    this.logger.Debug("Requesting WHOX a information");
+                    this.Send(new Message { Command = "WHO", Parameters = new[] { channelName, "%uhnatfc,001" } });
+
+                    // add the channel to the list of channels I'm in.
+                    this.channels.Add(channelName, new Channel(channelName));
+                }
+                else
+                {
+                    this.logger.DebugFormat("Seen {0} join channel {1}.", user, channelName);
+                    this.userCache.AddOrReplace(user.Nickname, (IrcUser)user);
+                    this.channels[channelName].Users.Add(user.Nickname, new ChannelUser((IrcUser)user, channelName));
+                }
+            }
+
+            if (e.Message.Command == Numerics.WhoXReply)
+            {
+                this.logger.WarnFormat("WHOX Reply:{0}", e.Message.Parameters.Implode());
+                this.HandleWhoXReply(e.Message);
+            }
+
+            if (e.Message.Command == Numerics.EndOfWho)
+            {
+                this.logger.Debug("End of who list.");
+                this.connectionRegistrationSemaphore.Release();
+            }
+        }
+
+        /// <summary>
+        /// The handle who x reply.
+        /// </summary>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        private void HandleWhoXReply(IMessage message)
+        {
+            if (message.Command != Numerics.WhoXReply)
+            {
+                throw new ArgumentException("Expected WHOX reply message", "message");
+            }
+
+            var parameters = message.Parameters.ToList();
+            if (parameters.Count() != 8)
+            {
+                throw new ArgumentException("Expected 8 WHOX parameters.", "message");
+            }
+
+            /* >> :holmes.freenode.net 354 stwalkerster 001 #wikipedia-en-accounts ChanServ services.           ChanServ       H@  0
+             * >> :holmes.freenode.net 354 stwalkerster 001 #wikipedia-en-accounts ~jamesur wikimedia/Jamesofur Jamesofur|away G  jamesofur
+             *                             .            t   c                      u        h                   n              f  a
+             *     prefix              cmd    0         1   2                      3        4                   5              6  7
+             */
+            var channel = parameters[2];
+            var user = parameters[3];
+            var host = parameters[4];
+            var nick = parameters[5];
+            var flags = parameters[6];
+            var away = flags[0] == 'G'; // H (here) / G (gone)
+            var modes = flags.Substring(1);
+            var account = parameters[7] == "0" ? "*" : parameters[7];
+
+            var ircUser = new IrcUser();
+            if (this.userCache.ContainsKey(nick))
+            {
+                ircUser = this.userCache[nick];
+            }
+            else
+            {
+                ircUser.Nickname = nick;
+                this.userCache.Add(nick, ircUser);
+            }
+
+            ircUser.Account = account;
+            ircUser.Username = user;
+            ircUser.Hostname = host;
+            ircUser.Away = away;
+
+         // TODO:   this.channels[channel].Users.Add();
+        }
+
+        #region Connection Registration
 
         /// <summary>
         /// The register connection.
@@ -224,11 +430,20 @@ namespace Helpmebot.IRC
                 return;
             }
 
+            if (message.Command == "NOTICE")
+            {
+                // do nothing, we don't care about these messages during registration.
+                return;
+            }
+
             // welcome to IRC!
             if (message.Command == Numerics.Welcome)
             {
                 this.logger.Info("Connection registration succeeded");
+                this.serverPrefix = message.Prefix;
                 this.connectionRegistered = true;
+                this.connectionRegistrationSemaphore.Release();
+
                 this.RaiseDataEvent(message);
                 return;
             }
@@ -245,6 +460,7 @@ namespace Helpmebot.IRC
             if (message.Command == "AUTHENTICATE")
             {
                 this.SaslAuth(message);
+                return;
             }
 
             // we've recieved a reply to our CAP commands
@@ -334,6 +550,13 @@ namespace Helpmebot.IRC
 
             if (message.Command == Numerics.SaslLoggedIn)
             {
+                this.logger.InfoFormat("You are now logged in as {2} ({1})", message.Parameters.ToArray());
+                this.loggedIn = true;
+                return;
+            }
+
+            if (message.Command == Numerics.SaslSuccess)
+            {
                 this.logger.InfoFormat("SASL Login succeeded.");
 
                 // logged in, continue with registration
@@ -346,13 +569,22 @@ namespace Helpmebot.IRC
             {
                 this.logger.WarnFormat("SASL Login failed.");
 
-                this.capSasl = false;
+                // not logged in, cancel sasl auth.
+                this.Send(new Message { Command = "AUTHENTICATE", Parameters = "*".ToEnumerable() });
+                return;
+            }
 
-                // not logged in, continue with registration
-                this.Send(new Message { Command = "CAP", Parameters = new List<string> { "END" } });
+            if (message.Command == Numerics.SaslAborted)
+            {
+                this.logger.WarnFormat("SASL Login aborted.");
+
+                // not logged in, cancel sasl auth.
+                this.Send(new Message { Command = "CAP", Parameters = "END".ToEnumerable() });
                 this.Send1459Registration();
                 return;
             }
+
+            this.logger.Error("How did I get here?");
         }
 
         /// <summary>
@@ -396,5 +628,7 @@ namespace Helpmebot.IRC
 
             this.Send(new Message { Command = "NICK", Parameters = this.Nickname.ToEnumerable() });
         }
+
+        #endregion
     }
 }
