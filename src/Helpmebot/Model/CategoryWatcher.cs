f937a8e7e01bc0a1181fa3d1b4713f491879a1ce
Simon Walker <github@stwalkerster.co.uk>
2014-01-22 12:21:26 +0000

    Category watcher repository stuff

---
+++ src/Helpmebot/Model/CategoryWatcher.cs
@@ -0,0 +1,50 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CategoryWatcher.cs" company="Helpmebot Development Team">
+//   Helpmebot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   Helpmebot is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace Helpmebot.Model
+{
+    using Helpmebot.Persistence;
+
+    /// <summary>
+    /// The category watcher.
+    /// </summary>
+    public class CategoryWatcher : EntityBase
+    {
+        #region Public Properties
+
+        /// <summary>
+        ///     Gets or sets the category.
+        /// </summary>
+        public virtual string Category { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the keyword.
+        /// </summary>
+        public virtual string Keyword { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the priority.
+        /// </summary>
+        public virtual int Priority { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the sleep time.
+        /// </summary>
+        public virtual int SleepTime { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/Helpmebot/Persistence/Mappings/CategoryWatcherMap.cs
@@ -0,0 +1,47 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CategoryWatcherMap.cs" company="Helpmebot Development Team">
+//   Helpmebot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   
+//   Helpmebot is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   
+//   You should have received a copy of the GNU General Public License
+//   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Helpmebot.Persistence.Mappings
+{
+    using FluentNHibernate.Mapping;
+
+    using Helpmebot.Model;
+
+    /// <summary>
+    /// The category watcher map.
+    /// </summary>
+    public class CategoryWatcherMap : ClassMap<CategoryWatcher>
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="CategoryWatcherMap"/> class.
+        /// </summary>
+        public CategoryWatcherMap()
+        {
+            this.Table("watcher");
+            this.Id(x => x.Id, "watcher_id");
+
+            this.Map(x => x.Category, "watcher_category");
+            this.Map(x => x.Keyword, "watcher_keyword");
+            this.Map(x => x.Priority, "watcher_priority");
+            this.Map(x => x.SleepTime, "watcher_sleeptime");
+        }
+
+        #endregion
+    }
+}
