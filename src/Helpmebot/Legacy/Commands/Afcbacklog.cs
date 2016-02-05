8fb67eab2fd66b729f3c9555334a198c8c8b84cc
Simon Walker <github@stwalkerster.co.uk>
2016-02-05 23:08:42 +0000

    Tweaked a few things:
    * Used a more sensible category size option, having seen what type the suggested one returned
    * Updated error message to not ping my misspelt nickname
    * Fixed class name and constructor name
    * Applied automatic code formatting

--- src/Helpmebot/Legacy/Commands/Afcbacklog.cs
+++ src/Helpmebot/Legacy/Commands/Afcbacklog.cs
@@ -1,15 +1,13 @@
 // --------------------------------------------------------------------------------------------------------------------
-// <copyright file="Afccount.cs" company="Helpmebot Development Team">
+// <copyright file="Afcbacklog.cs" company="Helpmebot Development Team">
 //   Helpmebot is free software: you can redistribute it and/or modify
 //   it under the terms of the GNU General Public License as published by
 //   the Free Software Foundation, either version 3 of the License, or
 //   (at your option) any later version.
-//
 //   Helpmebot is distributed in the hope that it will be useful,
 //   but WITHOUT ANY WARRANTY; without even the implied warranty of
 //   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //   GNU General Public License for more details.
-//
 //   You should have received a copy of the GNU General Public License
 //   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
 // </copyright>
@@ -17,17 +15,19 @@
 //   Returns the number of articles currently waiting at Articles for Creation
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
-
 namespace helpmebot6.Commands
 {
     using Helpmebot;
     using Helpmebot.Commands.Interfaces;
+    using Helpmebot.ExtensionMethods;
+    using Helpmebot.Legacy.Configuration;
     using Helpmebot.Legacy.Model;
+    using Helpmebot.Model;
 
     /// <summary>
     /// Returns the number of articles currently waiting at Articles for Creation
     /// </summary>
-    internal class Afccount : Categorysize
+    internal class Afcbacklog : Categorysize
     {
         /// <summary>
         /// Initialises a new instance of the <see cref="Afccount"/> class.
@@ -44,7 +44,7 @@ namespace helpmebot6.Commands
         /// <param name="commandServiceHelper">
         /// The message Service.
         /// </param>
-        public Afccount(LegacyUser source, string channel, string[] args, ICommandServiceHelper commandServiceHelper)
+        public Afcbacklog(LegacyUser source, string channel, string[] args, ICommandServiceHelper commandServiceHelper)
             : base(source, channel, args, commandServiceHelper)
         {
         }
@@ -57,36 +57,53 @@ namespace helpmebot6.Commands
         /// </returns>
         protected override CommandResponseHandler ExecuteCommand()
         {
-            int size = this.GetSizeOfCategory("Pending AfC submissions");
-            if(size = 0){
-              return new CommandResponseHandler("There are no new AfC submissions.");
+            string baseWiki = LegacyConfig.Singleton()["baseWiki", this.Channel];
+
+            MediaWikiSite mediaWikiSite = this.CommandServiceHelper.MediaWikiSiteRepository.GetById(int.Parse(baseWiki));
+            var size = mediaWikiSite.GetCategorySize("Pending AfC submissions");
+
+            if (size == 0)
+            {
+                // TODO: push these to the wiki?
+                return new CommandResponseHandler("There are no new AfC submissions.");
             }
-            else if (size < 200) {
+            else if (size < 200)
+            {
                 return new CommandResponseHandler("AfC is clearing out.");
             }
-            else if (size < 400) {
+            else if (size < 400)
+            {
                 return new CommandResponseHandler("There is a normal backlog at AfC.");
             }
-            else if (size < 650) {
+            else if (size < 650)
+            {
                 return new CommandResponseHandler("AfC is semi-backlogged at the moment.");
             }
-            else if (size < 900) {
+            else if (size < 900)
+            {
                 return new CommandResponseHandler("There is a backlog at AfC.");
             }
-            else if (size < 1200) {
+            else if (size < 1200)
+            {
                 return new CommandResponseHandler("AfC is highly backlogged at the moment.");
             }
-            else if (size < 2000) {
+            else if (size < 2000)
+            {
                 return new CommandResponseHandler("There is a severe backlog at AfC.");
             }
-            else if (size < 4000) {
+            else if (size < 4000)
+            {
                 return new CommandResponseHandler("AfC is critically backlogged.");
             }
-            else if (size < 10000) {
+            else if (size < 10000)
+            {
                 return new CommandResponseHandler("AfC is out of order.");
             }
-            else {
-                return new CommandResponseHandler("I am clueless to the backlog at AfC. Please contact my owner, stwalkster.");
+            else
+            {
+                return new CommandResponseHandler(
+                    "Unknown error occurred, current status of AfC is unknown.", 
+                    CommandResponseDestination.PrivateMessage);
             }
         }
     }
