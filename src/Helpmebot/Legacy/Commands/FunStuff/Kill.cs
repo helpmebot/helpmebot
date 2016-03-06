71e21b370bd7f59fb952e747d205216137e3bea6
Simon Walker <github@stwalkerster.co.uk>
2016-03-06 01:03:23 +0000

    convert brain commands to real commands

---
+++ src/Helpmebot/Legacy/Commands/FunStuff/Kill.cs
@@ -0,0 +1,60 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Kill.cs" company="Helpmebot Development Team">
+//   Helpmebot is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//   Helpmebot is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//   You should have received a copy of the GNU General Public License
+//   along with Helpmebot.  If not, see http://www.gnu.org/licenses/ .
+// </copyright>
+// <summary>
+//   Defines the Kill type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+namespace helpmebot6.Commands
+{
+    using Helpmebot.Commands.FunStuff;
+    using Helpmebot.Commands.Interfaces;
+    using Helpmebot.Legacy.Model;
+
+    /// <summary>
+    /// The kill.
+    /// </summary>
+    internal class Kill : ProtectedTargetedFunCommand
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Kill"/> class.
+        /// </summary>
+        /// <param name="source">
+        /// The source.
+        /// </param>
+        /// <param name="channel">
+        /// The channel.
+        /// </param>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        /// <param name="commandServiceHelper">
+        /// The message Service.
+        /// </param>
+        public Kill(LegacyUser source, string channel, string[] args, ICommandServiceHelper commandServiceHelper)
+            : base(source, channel, args, commandServiceHelper)
+        {
+        }
+
+        /// <summary>
+        /// Gets the target message.
+        /// </summary>
+        protected override string TargetMessage
+        {
+            get
+            {
+                return "CmdKill";
+            }
+        }
+    }
+}
