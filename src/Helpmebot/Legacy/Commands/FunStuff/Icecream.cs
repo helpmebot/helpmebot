05049c287b47404cf623502ccada4259eb00af9a
Simon Walker <github@stwalkerster.co.uk>
2018-07-12 19:54:09 +0100

    T329 migrate some commands from brain

---
+++ src/Helpmebot/Legacy/Commands/FunStuff/Icecream.cs
@@ -0,0 +1,18 @@
+﻿namespace helpmebot6.Commands
+{
+    using Helpmebot.Commands.FunStuff;
+    using Helpmebot.Commands.Interfaces;
+    using Helpmebot.Legacy.Model;
+
+    public class Icecream : TargetedFunCommand
+    {
+        public Icecream(LegacyUser source, string channel, string[] args, ICommandServiceHelper commandServiceHelper) : base(source, channel, args, commandServiceHelper)
+        {
+        }
+
+        protected override string TargetMessage
+        {
+            get { return "CmdIcecream"; }
+        }
+    }
+}
---
+++ src/Helpmebot/Legacy/Commands/FunStuff/Wine.cs
@@ -0,0 +1,18 @@
+﻿namespace helpmebot6.Commands
+{
+    using Helpmebot.Commands.FunStuff;
+    using Helpmebot.Commands.Interfaces;
+    using Helpmebot.Legacy.Model;
+
+    public class Wine : TargetedFunCommand
+    {
+        public Wine(LegacyUser source, string channel, string[] args, ICommandServiceHelper commandServiceHelper) : base(source, channel, args, commandServiceHelper)
+        {
+        }
+
+        protected override string TargetMessage
+        {
+            get { return "CmdWine"; }
+        }
+    }
+}
