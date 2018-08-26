3eb14fde7848e3e2c11bac6ea16d1bb542fcc2e7
Simon Walker <github@stwalkerster.co.uk>
2018-08-26 20:38:20 +0100

    Create NullCommand

---
+++ src/Helpmebot/Commands/NullCommand.cs
@@ -0,0 +1,39 @@
+namespace Helpmebot.Commands
+{
+    using System.Collections.Generic;
+    using Castle.Core.Logging;
+    using Stwalkerster.Bot.CommandLib.Attributes;
+    using Stwalkerster.Bot.CommandLib.Commands.CommandUtilities;
+    using Stwalkerster.Bot.CommandLib.Commands.CommandUtilities.Response;
+    using Stwalkerster.Bot.CommandLib.Model;
+    using Stwalkerster.Bot.CommandLib.Services.Interfaces;
+    using Stwalkerster.IrcClient.Interfaces;
+    using Stwalkerster.IrcClient.Model.Interfaces;
+
+    [CommandFlag(Flag.Standard)]
+    public class NullCommand : CommandBase
+    {
+        public NullCommand(
+            string commandSource,
+            IUser user,
+            IList<string> arguments,
+            ILogger logger,
+            IFlagService flagService,
+            IConfigurationProvider configurationProvider,
+            IIrcClient client) : base(
+            commandSource,
+            user,
+            arguments,
+            logger,
+            flagService,
+            configurationProvider,
+            client)
+        {
+        }
+
+        protected override IEnumerable<CommandResponse> Execute()
+        {
+            yield break;
+        }
+    }
+}
