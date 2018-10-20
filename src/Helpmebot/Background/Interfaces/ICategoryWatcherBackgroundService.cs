a7e2ff79e6631b735ad6f58f6a108a5c1eee5f60
Simon Walker <github@stwalkerster.co.uk>
2018-10-20 19:04:11 +0100

    Migrate FetchAllCommand

--- src/Helpmebot/Background/Interfaces/ICategoryWatcherBackgroundService.cs
+++ src/Helpmebot/Background/Interfaces/ICategoryWatcherBackgroundService.cs
@@ -1,9 +1,12 @@
 ﻿namespace Helpmebot.Background.Interfaces
 {
+    using System.Collections.Generic;
     using Helpmebot.Model;
 
     public interface ICategoryWatcherBackgroundService : ITimerBackgroundService
     {
         void ForceUpdate(string key, Channel destination);
+        IEnumerable<string> GetWatchedCategories(Channel destination);
+        IEnumerable<string> GetValidWatcherKeys();
     }
 }
