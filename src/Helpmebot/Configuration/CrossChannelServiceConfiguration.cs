80fc2af694b9c80b87b1b4495bcd324100cda90a
Simon Walker <github@stwalkerster.co.uk>
2018-10-21 03:51:05 +0100

    Cross-channel rate limiting

---
+++ src/Helpmebot/Configuration/CrossChannelServiceConfiguration.cs
@@ -0,0 +1,14 @@
+namespace Helpmebot.Configuration
+{
+    public class CrossChannelServiceConfiguration
+    {
+        public CrossChannelServiceConfiguration(int rateLimitMax, int rateLimitDuration)
+        {
+            this.RateLimitMax = rateLimitMax;
+            this.RateLimitDuration = rateLimitDuration;
+        }
+
+        public int RateLimitMax { get; private set; }
+        public int RateLimitDuration { get; private set; }
+    }
+}
--- src/Helpmebot/Services/CrossChannelService.cs
+++ src/Helpmebot/Services/CrossChannelService.cs
@@ -4,7 +4,9 @@ namespace Helpmebot.Services
     using System.Collections.Generic;
     using System.Linq;
     using Castle.Core.Logging;
+    using FluentNHibernate.Utils;
     using Helpmebot.Commands.CrossChannel;
+    using Helpmebot.Configuration;
     using Helpmebot.Model;
     using Helpmebot.Services.Interfaces;
     using NHibernate;
@@ -16,29 +18,41 @@ namespace Helpmebot.Services
     public class CrossChannelService : CommandParserProviderServiceBase<CrossChannel>, ICrossChannelService
     {
         private readonly ISession databaseSession;
+        private readonly CrossChannelServiceConfiguration config;
         private readonly object sessionLock = new object();
+        
+        private static readonly Dictionary<string, RateLimitCacheEntry> RateLimitCache =
+            new Dictionary<string, RateLimitCacheEntry>();
 
-        public CrossChannelService(ISession databaseSession, ILogger logger, ICommandParser commandParser)
+        public CrossChannelService(
+            ISession databaseSession,
+            ILogger logger,
+            ICommandParser commandParser,
+            CrossChannelServiceConfiguration config)
             : base(commandParser, logger)
         {
             this.databaseSession = databaseSession;
+            this.config = config;
         }
 
         public void Configure(Channel frontend, Channel backend, ISession localSession)
         {
             var existing = localSession.CreateCriteria<CrossChannel>()
-                .Add(Restrictions.Or(
+                .Add(
                     Restrictions.Or(
-                        Restrictions.Eq("FrontendChannel", frontend),
-                        Restrictions.Eq("FrontendChannel", backend)),
-                    Restrictions.Or(
-                        Restrictions.Eq("BackendChannel", frontend),
-                        Restrictions.Eq("BackendChannel", backend))
-                )).List<CrossChannel>();
+                        Restrictions.Or(
+                            Restrictions.Eq("FrontendChannel", frontend),
+                            Restrictions.Eq("FrontendChannel", backend)),
+                        Restrictions.Or(
+                            Restrictions.Eq("BackendChannel", frontend),
+                            Restrictions.Eq("BackendChannel", backend))
+                    ))
+                .List<CrossChannel>();
 
             if (existing.Any())
             {
-                throw new Exception("At least one of the channels requested is already involved in a cross-channel configuration.");
+                throw new Exception(
+                    "At least one of the channels requested is already involved in a cross-channel configuration.");
             }
 
             var cc = new CrossChannel
@@ -51,7 +65,7 @@ namespace Helpmebot.Services
 
             localSession.Save(cc);
         }
-        
+
         public void Deconfigure(Channel backend, ISession localSession)
         {
             var existing = localSession.CreateCriteria<CrossChannel>()
@@ -92,12 +106,12 @@ namespace Helpmebot.Services
             {
                 throw new Exception("Cannot enable notifications before keyword is configured.");
             }
-            
+
             if (status && string.IsNullOrWhiteSpace(existing.NotifyMessage))
             {
                 throw new Exception("Cannot enable notifications before message is configured.");
             }
-            
+
             existing.NotifyEnabled = status;
             localSession.Update(existing);
 
@@ -110,7 +124,7 @@ namespace Helpmebot.Services
                 this.UnregisterCommand(existing);
             }
         }
-        
+
         public void NotificationMessage(Channel backend, string message, ISession localSession)
         {
             var existing = localSession.CreateCriteria<CrossChannel>()
@@ -127,11 +141,11 @@ namespace Helpmebot.Services
                 // no-op
                 return;
             }
-  
+
             existing.NotifyMessage = message;
             localSession.Update(existing);
         }
-        
+
         public void NotificationKeyword(Channel backend, string keyword, ISession localSession)
         {
             var existing = localSession.CreateCriteria<CrossChannel>()
@@ -153,7 +167,7 @@ namespace Helpmebot.Services
             {
                 throw new Exception("Cannot change notification keyword while notifications are enabled.");
             }
-            
+
             existing.NotifyKeyword = keyword;
             localSession.Update(existing);
         }
@@ -170,11 +184,17 @@ namespace Helpmebot.Services
                 throw new Exception("Cannot find cross-channel configuration for this channel.");
             }
 
+            if (this.RateLimit(user, existing.Id))
+            {
+                this.Logger.InfoFormat("Rate limited notification response for {0} in {1}", user, frontend);
+                return;
+            }
+            
             client.SendNotice(
                 existing.BackendChannel.Name,
                 string.Format(existing.NotifyMessage, user.Nickname, frontend.Name, message));
         }
-        
+
         protected override IList<CrossChannel> ItemsToRegister()
         {
             IList<CrossChannel> itemsToRegister;
@@ -187,10 +207,77 @@ namespace Helpmebot.Services
 
             return itemsToRegister;
         }
-        
+
         protected override Type CommandImplementation()
         {
             return typeof(CrossChannelNotifyCommand);
+        }
+
+        private bool RateLimit(IUser source, int id)
+        {
+            if (source == null || source.Hostname == null)
+            {
+                this.Logger.Error("Rate limiting called with no source or no source hostname!");
+                return true;
+            }
+
+            var cacheKey = string.Format("{1}|{0}", source.Hostname, id);
+
+            lock (RateLimitCache)
+            {
+                if (RateLimitCache.ContainsKey(cacheKey))
+                {
+                    this.Logger.Debug("Rate limit key found.");
+
+                    var cacheEntry = RateLimitCache[cacheKey];
+
+                    if (cacheEntry.Expiry.AddMinutes(this.config.RateLimitDuration) >= DateTime.Now)
+                    {
+                        this.Logger.Debug("Rate limit key NOT expired.");
+
+                        if (cacheEntry.Counter >= this.config.RateLimitMax)
+                        {
+                            this.Logger.Debug("Rate limit HIT");
+
+                            // RATE LIMITED!
+                            return true;
+                        }
+
+                        this.Logger.Debug("Rate limit incremented.");
+
+                        // increment counter
+                        cacheEntry.Counter++;
+                    }
+                    else
+                    {
+                        this.Logger.Debug("Rate limit key is expired, resetting to new value.");
+
+                        // Cache expired
+                        cacheEntry.Expiry = DateTime.Now;
+                        cacheEntry.Counter = 1;
+                    }
+                }
+                else
+                {
+                    this.Logger.Debug("Rate limit not found, creating key.");
+
+                    // Not in cache.
+                    var cacheEntry = new RateLimitCacheEntry {Expiry = DateTime.Now, Counter = 1};
+                    RateLimitCache.Add(cacheKey, cacheEntry);
+                }
+
+                // Clean up the cache
+                foreach (var key in RateLimitCache.Keys.ToList())
+                {
+                    if (RateLimitCache[key].Expiry.AddMinutes(this.config.RateLimitDuration) < DateTime.Now)
+                    {
+                        // Expired.
+                        RateLimitCache.Remove(key);
+                    }
+                }
+            }
+
+            return false;
         }
     }
 }
