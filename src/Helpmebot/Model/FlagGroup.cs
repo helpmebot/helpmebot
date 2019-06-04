3962674550bb5b80c1e7cf427479258e2d1fd777
Simon Walker <github@stwalkerster.co.uk>
2019-06-05 00:56:07 +0100

    add flag group eqality comparer

--- src/Helpmebot/Model/FlagGroup.cs
+++ src/Helpmebot/Model/FlagGroup.cs
@@ -35,5 +35,42 @@ namespace Helpmebot.Model
         {
             return string.Format(@"{0} {{{2}{1}}}", this.Name, this.Flags, this.Mode);
         }
+
+        protected bool Equals(FlagGroup other)
+        {
+            return string.Equals(this.Name, other.Name) && string.Equals(this.Flags, other.Flags) && string.Equals(this.Mode, other.Mode) && this.LastModified.Equals(other.LastModified);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != this.GetType())
+            {
+                return false;
+            }
+
+            return Equals((FlagGroup) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (this.Name != null ? this.Name.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (this.Flags != null ? this.Flags.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (this.Mode != null ? this.Mode.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ this.LastModified.GetHashCode();
+                return hashCode;
+            }
+        }
     }
 }
