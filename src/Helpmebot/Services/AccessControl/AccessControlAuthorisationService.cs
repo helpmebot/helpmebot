61a3c04fea5bcfe7f06b865162aa5c561af2f9c6
Simon Walker <github@stwalkerster.co.uk>
2019-06-04 21:57:21 +0100

    Debugging messages

--- src/Helpmebot/Services/AccessControl/AccessControlAuthorisationService.cs
+++ src/Helpmebot/Services/AccessControl/AccessControlAuthorisationService.cs
@@ -41,14 +41,19 @@ namespace Helpmebot.Services.AccessControl
 
             var flagGroups = matchingUsers.SelectMany(x => x.AppliedFlagGroups).ToList();
 
+            this.logger.DebugFormat("Fetched flag groups for {0}", user);
+
             this.logger.DebugFormat(
                 "Found flag groups {0} apply to {1}",
                 flagGroups.Distinct().Aggregate("", (s, g) => s + "," + g.Name).TrimStart(','),
                 user);
 
+            this.logger.DebugFormat("Aggregating additions for {0}", user);
             var changes = flagGroups.Where(x => x.Mode == "+").Aggregate("+", (s, group) => s + group.Flags);
+            this.logger.DebugFormat("Aggregating subtractions for {0} to {1}", user, changes);
             changes = flagGroups.Where(x => x.Mode == "-").Aggregate(changes + "-", (s, group) => s + group.Flags);
 
+            this.logger.DebugFormat("Applying changes for resultant set [{1}] for {0}", user, changes);
             var resultantSet =
                 AccessControlManagementService.ApplyFlagChangesToFlags(
                     ref changes,
