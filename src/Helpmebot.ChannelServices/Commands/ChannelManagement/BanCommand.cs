bd3c6c9eb360513de7418437ab0ed162c8a7f1d2
Simon Walker <github@stwalkerster.co.uk>
2021-02-13 14:45:32 +0000

    add capability to ban, extend tracked ip ranges, add unvoiced users to tracking when they match the badwords list

---
+++ src/Helpmebot.ChannelServices/Commands/ChannelManagement/BanCommand.cs
@@ -0,0 +1,47 @@
+namespace Helpmebot.ChannelServices.Commands.ChannelManagement
+{
+    using System.Collections.Generic;
+    using Castle.Core.Logging;
+    using Helpmebot.ChannelServices.Services.Interfaces;
+    using Helpmebot.CoreServices.Attributes;
+    using Helpmebot.CoreServices.Model;
+    using Stwalkerster.Bot.CommandLib.Attributes;
+    using Stwalkerster.Bot.CommandLib.Commands.CommandUtilities;
+    using Stwalkerster.Bot.CommandLib.Commands.CommandUtilities.Response;
+    using Stwalkerster.Bot.CommandLib.Services.Interfaces;
+    using Stwalkerster.IrcClient.Interfaces;
+    using Stwalkerster.IrcClient.Model.Interfaces;
+
+    [CommandFlag(Flags.Configuration)]
+    [Undocumented]
+    public class EnactBanCommand : CommandBase
+    {
+        private readonly ITrollMonitoringService trollMonitoringService;
+
+        public EnactBanCommand(
+            string commandSource,
+            IUser user,
+            IList<string> arguments,
+            ILogger logger,
+            IFlagService flagService,
+            IConfigurationProvider configurationProvider,
+            IIrcClient client,
+            ITrollMonitoringService trollMonitoringService) : base(
+            commandSource,
+            user,
+            arguments,
+            logger,
+            flagService,
+            configurationProvider,
+            client)
+        {
+            this.trollMonitoringService = trollMonitoringService;
+        }
+
+        protected override IEnumerable<CommandResponse> Execute()
+        {
+            this.trollMonitoringService.EnactBan();
+            yield break;
+        }
+    }
+}
