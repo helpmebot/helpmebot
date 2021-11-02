11b6dbd76a429f6a4a3369ee738aa5dd3c5c9ca0
Simon Walker <github@stwalkerster.co.uk>
2021-11-02 18:03:15 +0000

    Bin unused configuration

--- src/Helpmebot.Configuration/BotConfiguration.cs
+++ src/Helpmebot.Configuration/BotConfiguration.cs
@@ -7,19 +7,10 @@ namespace Helpmebot.Configuration
     {
         public string CommandTrigger { get; set; }
         public string UserAgent { get; set; }
-        public int HttpTimeout { get; set; }
         public string DebugChannel { get; set; }
-        [Obsolete]
-        public bool EnableNotificationService { get; set; }
         public bool DisableCertificateValidation { get; set; }
-        [Obsolete]
-        public string GoogleApiKey { get; set; }
         public string IpInfoDbApiKey { get; set; }
         public string MaxMindDatabasePath { get; set; }
-        [Obsolete]
-        public string AccDeploymentPassword { get; set; }
-        [Obsolete]
-        public int? SystemMonitoringPort { get; set; }
         public int? PrometheusMetricsPort { get; set; }
         public string Log4NetConfiguration { get; set; }
     }
