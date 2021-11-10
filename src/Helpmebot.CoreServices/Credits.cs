8456124b9e487c1e1eaf93617c72feecee7f3834
Simon Walker <github@stwalkerster.co.uk>
2021-11-10 00:33:05 +0000

    Add Credits file (autogenerated on build)

---
+++ src/Helpmebot.CoreServices/Credits.cs
@@ -0,0 +1,12 @@
+// ReSharper disable All
+namespace Helpmebot.CoreServices
+{
+    using System.Collections.Generic;
+
+    public static class Credits
+    {
+        // The next line of code is matched automatically and replaced by the build system.
+        // DO NOT MODIFY THIS LINE
+        public static Dictionary<string, uint> Authors => new Dictionary<string, uint> { };
+    }
+}
