f49fa011bd2c7e8c4ec551bc25fe718dcfb33a0c
methecooldude <rsmithy@cluenet.org>
2021-11-14 16:23:34 +0000

    New Hot Chocolate Command
    Resolves https://phabricator.stwalkerster.co.uk/T1563

---
+++ src/Helpmebot.Commands/Commands/FunCommands/Commands/HotChocCommand.cs
@@ -0,0 +1,48 @@
+namespace Helpmebot.Commands.Commands.FunCommands.Commands
+{
+    using System.Collections.Generic;
+    using Castle.Core.Logging;
+    using Helpmebot.Commands.Commands.FunCommands;
+    using Helpmebot.CoreServices.Model;
+    using Helpmebot.CoreServices.Services.Interfaces;
+    using Helpmebot.Model;
+    using NHibernate;
+    using Stwalkerster.Bot.CommandLib.Attributes;
+    using Stwalkerster.Bot.CommandLib.Commands.CommandUtilities.Response;
+    using Stwalkerster.Bot.CommandLib.Services.Interfaces;
+    using Stwalkerster.IrcClient.Interfaces;
+    using Stwalkerster.IrcClient.Model.Interfaces;
+
+    [CommandInvocation("HotChocolate")]
+    [CommandFlag(Flags.Fun)]
+    public class HotChocCommand : TargetedFunCommandBase
+    {
+        public HotChocCommand(
+            string commandSource,
+            IUser user,
+            IList<string> arguments,
+            ILogger logger,
+            IFlagService flagService,
+            IConfigurationProvider configurationProvider,
+            IIrcClient client,
+            ISession databaseSession,
+            IMessageService messageService) : base(
+            commandSource,
+            user,
+            arguments,
+            logger,
+            flagService,
+            configurationProvider,
+            client,
+            databaseSession,
+            messageService)
+        {
+        }
+
+        [Help(new[] {"", "<user>"}, "Gives a user a hot chocolate.")]
+        protected override IEnumerable<CommandResponse> Execute()
+        {
+            return this.GetResponse("cmdHotChocolate");
+        }
+    }
+}
