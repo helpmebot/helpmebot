7a26f43b7b90b4b11c4e91c3e8e2799b7ae101f3
Simon Walker <github@stwalkerster.co.uk>
2021-11-16 01:33:52 +0000

    Add file-based message store

--- src/Helpmebot.CoreServices/Services/Messages/FileMessageRepository.cs
+++ src/Helpmebot.CoreServices/Services/Messages/FileMessageRepository.cs
@@ -1,15 +1,42 @@
 namespace Helpmebot.CoreServices.Services.Messages
 {
-    using System;
     using System.Collections.Generic;
+    using System.IO;
+    using YamlDotNet.Serialization;
 
     public class FileMessageRepository : ReadOnlyMessageRepository
     {
+        private readonly Dictionary<string, List<List<string>>> strings = new Dictionary<string, List<List<string>>>();
+        public FileMessageRepository()
+        {
+            var fileNames = Directory.GetFiles("Messages/");
+            var deserializer = new DeserializerBuilder().Build();
+            
+            foreach (var file in fileNames)
+            {
+                var data = deserializer.Deserialize<Dictionary<string,List<List<string>>>>(File.ReadAllText(file));
+
+                if (data == null)
+                {
+                    continue;
+                } 
+                
+                foreach (var kvp in data)
+                {
+                    this.strings.Add(kvp.Key, kvp.Value);
+                }
+            }
+        }
+        
         public override bool SupportsContext => false;
         
         public override List<List<string>> Get(string key, string contextType, string context)
         {
-            // FIXME: implement:
+            if (this.strings.ContainsKey(key))
+            {
+                return this.strings[key];
+            }
+
             return null;
         }
     }
