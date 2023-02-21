7a622e04d6b9845b3e7b5b84cd2d8fb298a08e92
Simon Walker <github@stwalkerster.co.uk>
2023-02-21 18:56:19 +0000

    Fix mapping bug

--- src/Helpmebot.CoreServices/Persistence/Mappings/CrossChannelMap.cs
+++ src/Helpmebot.CoreServices/Persistence/Mappings/CrossChannelMap.cs
@@ -9,8 +9,8 @@ namespace Helpmebot.Persistence.Mappings
         {
             this.Table("crosschannel");
             this.Id(x => x.Id, "id");
-            this.Map(x => x.FrontendChannel, "frontend_id");
-            this.Map(x => x.BackendChannel, "backend_id");
+            this.Map(x => x.FrontendChannel, "frontend_name");
+            this.Map(x => x.BackendChannel, "backend_name");
             this.Map(x => x.NotifyEnabled, "notify_enabled");
             this.Map(x => x.NotifyKeyword, "notify_keyword");
             this.Map(x => x.NotifyMessage, "notify_message");
